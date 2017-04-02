288e2d7a0afe6abbc77af4a34b5bd1bd388bd7b0
Jeffrey-Young <youngjt@live.unc.edu>
2017-04-02 14:15:49 -0400

    ready for demo

--- src/comp110/Main.java
+++ src/comp110/Main.java
@@ -11,36 +11,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
-public class Main extends Application{
+public class Main extends Application {
 	
-	Stage _stage;
-	Circle _circle;
 	
-	public static void main(String[] args){
-		Application.launch(args);
-	}
-	
-	public void start(Stage stage){
-		_stage = stage;
-		BorderPane pane = new BorderPane();
-		Scene scene = new Scene(pane);
-		stage.setScene(scene);
-		stage.setTitle("COMP523");
-		stage.setHeight(300);
-		stage.setWidth(300);
-		Button button = new Button("Click here");
-		//button.setOnAction((e) -> new Alert(AlertType.INFORMATION, "Button Pressed").show());
-		button.setOnAction(this::changeColor);
-		pane.setBottom(button);
-		
-		_circle = new Circle(50.0, Color.RED);
-		pane.setTop(_circle);
-		
-		stage.show();
-		
-	}
-	
-	private void changeColor(ActionEvent e){
-		_circle.setFill(new Color(Math.random(), Math.random(), Math.random(), 1));
-	}
 }
